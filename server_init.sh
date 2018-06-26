30188b66e43c21f81e62101981103b594bbb29d8
mlch911 <mlch911@mluoc.tech>
2018-06-27 03:25:02 +0800

    Ver 0.0.3
    bug fix

--- server_init.sh
+++ server_init.sh
@@ -2,5 +2,5 @@ yum -y install wget nano
 yum -y update
 github="https://git.mluoc.tk/mlch911/server_init/raw/branch/master"
 cd /root
-wget wget --no-check-certificate -qO- "${github}/ssh_pub_keys
+wget wget --no-check-certificate -qO- ${github}/ssh_pub_keys
 echo /root/ssh_pub_keys >> /root/.ssh/authorized_keys
