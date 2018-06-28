22d2e394b1afc2f60283e47c301cf22a504b280d
mlch911 <mlch911@mluoc.tech>
2018-06-29 02:33:57 +0800

    Ver 0.0.9
    add HTML-speedtest

--- server_init.sh
+++ server_init.sh
@@ -5,7 +5,7 @@ export PATH
 #=================================================
 #	System Required: CentOS 7+
 #	Description: 服务器初始化脚本
-#	Version: 0.5.3
+#	Version: 0.0.9
 #	Author: 壕琛
 #	Blog: http://mluoc.top/
 #=================================================
@@ -20,11 +20,11 @@ wget --no-check-certificate -qO- -O ssh_pub_keys ${github}/ssh_pub_keys
 
 if test ! -e ${dir}
 	then mkdir ${dir}
-	chmod 700 $1
+	chmod +x $1
 fi
 if test ! -e ${dir}/${file}
 	then touch ${dir}/${file}
-	chmod 600 ${dir}/${file}
+	chmod +x ${dir}/${file}
 fi
 cat /root/ssh_pub_keys | while read line; do echo ${line} >> ${dir}/${file} ; done
-docker run -d -p 6688:80 ilemonrain/html5-speedtest:alpine
+docker run -d --name=speedtest -p 6688:80 ilemonrain/html5-speedtest:alpine
