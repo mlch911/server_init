05d0062a18fa2f3cf805f5cced63ae6ef56cde14
mlch911 <mlch911@mluoc.tech>
2018-10-12 02:26:18 +0800

    Ver 0.1.3
    add function for changing ssh port

--- server_init.sh
+++ server_init.sh
@@ -28,3 +28,37 @@ if test ! -e ${dir}/${file}
 fi
 cat /root/ssh_pub_keys | while read line; do echo ${line} >> ${dir}/${file} ; done
 # docker run -d --name=speedtest -p 6688:80 ilemonrain/html5-speedtest:alpine
+
+# 更改ssh端口
+read -p "是否更改ssh端口 :(y/n)" input_a
+if [ ${input_a} == "y" ] ;then
+	cd /etc/ssh/
+	read -p "新的ssh端口 :" ssh_port
+	sed -i "17c Port ${ssh_port}" ssh_config
+	read -p "修改完成，是否开放防火墙 :(y/n)" input_b
+	if [ ${input_b} == "y" ] ;then
+		echo -e " 请选择防火墙类型 :
+		${Green_font_prefix}1.${Font_color_suffix} firewalld
+		${Green_font_prefix}2.${Font_color_suffix} iptables
+		————————————————————————————————"
+		read -p "请输入数字 :" num
+		if [ ${num} == "1" ] ;then
+			firewall-cmd --permanent --zone=public --add-port=${ssh_port}/tcp
+			firewall-cmd --permanent --zone=public --add-port=${ssh_port}/udp
+			firewall-cmd --reload
+		elif [ ${num} == "2" ] ;then
+			iptables -A INPUT -p tcp --dport ${ssh_port} -j ACCEPT
+			iptables -A INPUT -p udp --dport ${ssh_port} -j ACCEPT
+			service iptables save
+			service iptables restart
+		fi
+	fi
+	read -p "是否重启ssh服务 :(y/n)" input_c
+	if [ ${input_c} == "y" ] ;then
+		systemctl restart sshd.service
+	fi
+	echo -e "ssh端口更改完成！"
+fi
+
+clear
+echo -e "Server initialzation finished!"
