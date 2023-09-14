fcb690816c62905bdf2ca4f8473c38ccba4bb6f2
mlch911 <mlch911@mluoc.tech>
2023-09-14 19:09:09 +0800

    Update Docker

--- server_init.sh
+++ server_init.sh
@@ -135,6 +135,8 @@ Init_Shell() {
 	# install_mosh
 	install_lazygit
 
+	# TODO: https://stackoverflow.com/questions/73397110/how-to-stop-ubuntu-pop-up-daemons-using-outdated-libraries-when-using-apt-to-i
+
 	gem install colorls
 
 	# config
@@ -308,9 +310,15 @@ install_node() {
 		yum install -y nodejs
 		;;
 	"debian" | "ubuntu")
-		apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
-		curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-		sudo apt-get install -y nodejs
+		# https://github.com/nodesource/distributions#debian-and-ubuntu-based-distributions
+		sudo apt-get update
+		sudo apt-get install -y ca-certificates curl gnupg
+		sudo mkdir -p /etc/apt/keyrings
+		curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+		NODE_MAJOR=20
+		echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+		sudo apt-get update
+		sudo apt-get install nodejs -y
 		;;
 	*)
 		echo "安装NodeJS失败"
